619155edf2911a128577c88f0f1c7f56765497c7
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-08-25 11:00:32 -0300

    Agregar archivos de proyecto.

---
+++ Customer/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using Customer.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Customer.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Get()
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["CustomerConnection"].ConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(@"Select [Id], [CusId], [CusName] FROM [dbo].[CustomerInfo] WHERE [Status] <> 0", connection))
+                {
+                    command.Notification = null;
+                    SqlDependency dependency = new SqlDependency(command);
+                    dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
+                    if (connection.State == ConnectionState.Closed)
+                        connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    var listCus = reader.Cast<IDataRecord>()
+                        .Select(x => new
+                        {
+                            Id = (int)x["Id"],
+                            CusId = (string)x["CusId"],
+                            CusName = (string)x["CusName"],
+                        }).ToList();
+
+                    return Json(new { listCus = listCus }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        private void dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            CusHub.Show();
+        }
+    }
+}
